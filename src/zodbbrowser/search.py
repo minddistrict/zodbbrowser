fac74f9c4cefd10ae5236cbd07fe28fef8d35031
Sylvain Viollon <sviollon@minddistrict.com>
2016-09-16 10:24:48 +0200

    Add a report option to give you the OID of the record that contains broken classes.

--- src/zodbbrowser/search.py
+++ src/zodbbrowser/search.py
@@ -9,6 +9,10 @@ import ZODB.broken
 from zodbbrowser.standalone import open_database
 
 
+class BrokenRecord(ValueError):
+    pass
+
+
 def is_broken(symb):
     """Return True if the given symbol is broken."""
     return (isinstance(symb, types.TypeType) and
@@ -17,13 +21,16 @@ def is_broken(symb):
 
 class Search(object):
 
-    def __init__(self, classes=[]):
+    def __init__(self, classes=[], report=False):
         self.classes = classes
+        self.report = report
 
     def validate(self, cls_info, cls):
         fullname = '.'.join(cls_info)
         if is_broken(cls):
             print 'Broken class {}'.format(fullname)
+            if self.report:
+                raise BrokenRecord()
         if self.classes and fullname in self.classes:
             print 'Instance of {} found'.format(fullname)
 
@@ -63,8 +70,8 @@ def main(args=None):
     parser = optparse.OptionParser(
         'usage: %prog [options] [--db DATA.FS | --zeo ADDRESS | --config FILE]',
         prog='zodbsearch',
-        description='Search if one or multiple Python class are used in '
-            'a database.')
+        description=('Search if one or multiple Python class are used in '
+                     'a database.'))
     parser.add_option('--config', metavar='FILE',
                       help='use a ZConfig file to specify database')
     parser.add_option('--zeo', metavar='ADDRESS',
@@ -77,18 +84,25 @@ def main(args=None):
     parser.add_option('--data', action="store_true",
                       dest="data", default=False,
                       help='check inside persisted data too')
+    parser.add_option('--report', action="store_true",
+                      dest="report", default=False,
+                      help='report record OID in case of broken class')
     opts, args = parser.parse_args(args)
     try:
         db = open_database(opts)
     except ValueError as error:
         parser.error(error.args[0])
 
-    search = Search(args)
+    search = Search(args, report=opts.report)
     for transaction in db._storage.iterator():
         for record in transaction:
-            unpickler = cPickle.Unpickler(io.BytesIO(record.data))
-            unpickler.persistent_load = search.persistent_load
-            unpickler.find_global = search.find_global
-            search.read_class_meta(unpickler.load())
-            if opts.data:
-                unpickler.load()
+            try:
+                unpickler = cPickle.Unpickler(io.BytesIO(record.data))
+                unpickler.persistent_load = search.persistent_load
+                unpickler.find_global = search.find_global
+                search.read_class_meta(unpickler.load())
+                if opts.data:
+                    unpickler.load()
+            except BrokenRecord:
+                print 'ZODB record "0x{:x}" contains broken classes.'.format(
+                    ZODB.utils.u64(record.oid))
